af3f792a674e1191beafdff8030294edef629579
Flavio Nogueira <flavionogueirabarros@gmail.com>
2022-06-10 00:12:05 -0300

    refactor: Adapter for customer and appointment, pending tests

---
+++ src/tests/controller/customer.test.ts
@@ -0,0 +1,9 @@
+import CustomerController from "../../controller/customerController";
+
+
+describe('CustomerController', () => {
+    it('should be defined', () => {
+        expect(CustomerController).toBeDefined();
+    }
+    );
+});
