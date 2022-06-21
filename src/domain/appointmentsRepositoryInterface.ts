4d2c9739976e3b5907af69ae3e474efef5a67c41
Flavio Nogueira <flavionogueirabarros@gmail.com>
2022-06-21 07:57:42 -0300

    refactor: clean arch adding repositories

---
+++ src/domain/appointmentsRepositoryInterface.ts
@@ -0,0 +1,8 @@
+import Appointment from "./appointments";
+
+export default interface AppointmentsRepositoryInterface {
+    insert(appointment: Appointment): Promise<void>
+    update(appointment: Appointment, id: string): Promise<void>
+    getAllAppointments(): Promise<Appointment[]>
+    getAppointmentById(id: string): Promise<Appointment>
+}
