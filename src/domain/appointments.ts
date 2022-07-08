dca14c840ea549728ee1051a5f58281faeb81de5
Flavio Nogueira <flavionogueirabarros@gmail.com>
2022-07-08 20:37:38 -0300

    refactor: domain and interface contracts

--- src/domain/appointments.ts
+++ src/domain/appointments.ts
@@ -1,9 +1,45 @@
-import { IAppointment } from './interfaces'
+import AppointmentsRepositoryInterface from './appointmentsRepositoryInterface'
 
-export default class Appointment implements IAppointment {
-  public id?: string
-  public date: Date
-  public serviceDoneAt: Date
-  public notes: string
-  public isDone: boolean
+export default abstract class Appointment
+  implements AppointmentsRepositoryInterface
+{
+  abstract insert(appointment: Appointment): Promise<void>
+  abstract update(appointment: Appointment, id: string): Promise<void>
+  abstract getAllAppointments(): Promise<Appointment[]>
+  abstract getAppointmentById(id: string): Promise<Appointment>
+  private _id?: string | undefined
+  public get id(): string | undefined {
+    return this._id
+  }
+  public set id(value: string | undefined) {
+    this._id = value
+  }
+  private _date: Date
+  public get date(): Date {
+    return this._date
+  }
+  public set date(value: Date) {
+    this._date = value
+  }
+  private _serviceDoneAt: Date
+  public get serviceDoneAt(): Date {
+    return this._serviceDoneAt
+  }
+  public set serviceDoneAt(value: Date) {
+    this._serviceDoneAt = value
+  }
+  private _notes: string
+  public get notes(): string {
+    return this._notes
+  }
+  public set notes(value: string) {
+    this._notes = value
+  }
+  private _isDone: boolean
+  public get isDone(): boolean {
+    return this._isDone
+  }
+  public set isDone(value: boolean) {
+    this._isDone = value
+  }
 }
--- src/domain/customer.ts
+++ src/domain/customer.ts
@@ -1,10 +1,38 @@
-import { ICustomer } from './interfaces'
+import CustomerRepositoryInterface from "./customerRepositoryInterface"
 
-export default class Customer implements ICustomer {
-  public id?: String = ''
-  constructor(
-    public name: String = '',
-    public registrationId: String = '',
-    public birthdate: Date
-  ) {}
+export default abstract class Customer implements CustomerRepositoryInterface {
+  abstract insert(customer: Customer): Promise<void>
+  abstract update(customer: Customer, id: string): Promise<void>
+  abstract getAllCustomers(): Promise<Customer[]>
+  abstract getCustomerById(id: string): Promise<Customer>
+
+  private _name: string
+  private _registrationId: string
+  private _birthdate: Date
+
+  public get registrationId(): string {
+    return this._registrationId
+  }
+  public set registrationId(value: string) {
+    this._registrationId = value
+  }
+  public get birthdate(): Date {
+    return this._birthdate
+  }
+  public set birthdate(value: Date) {
+    this._birthdate = value
+  }
+  public get name(): string {
+    return this._name
+  }
+  public set name(value: string) {
+    this._name = value
+  }
+  private _id?: string | undefined = ''
+  public get id(): string | undefined {
+    return this._id
+  }
+  public set id(value: string | undefined) {
+    this._id = value
+  }
 }
